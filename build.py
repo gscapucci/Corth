b99bbc741582bb57c89b7a1b538a76680d44bfdd
gscapucci <guilherme_capucci@outlook.com>
2022-05-24 12:37:40 -0300

    Python script to build the project

---
+++ build.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+def run_echoed_cmd(args):
+    print('[CMD] ' + args)
+    args = args.split()
+    if subprocess.run(args).returncode != 0:
+        print("error")
+        exit(1)
+
+def main():
+    run_echoed_cmd('gcc corth.c main.c -o corth -lm')
+    run_echoed_cmd('gcc -g corth.c main.c -lm')
+
+if __name__ == "__main__":
+    main()
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-run':
+            run_echoed_cmd('./corth std.corth')
+            run_echoed_cmd('fasm std.fasm')
+            run_echoed_cmd('./std')
+
